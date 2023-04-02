487aa0304e3bd3301222cc3f8bbbaca2369d0a0a
yeshaojun <yeshaojun5056@163.com>
2023-04-02 23:37:12 +0800

    feat: init

--- packages/cli/__tests__/cli.test.js
+++ packages/cli/__tests__/cli.test.js
@@ -1,7 +1,39 @@
-'use strict';
+import { execa } from "execa";
+import path from "node:path";
 
-const cli = require('..');
+const CLI = path.join(__dirname, "../bin/cli.js");
+const bin =
+  () =>
+  (...args) =>
+    execa(CLI, args);
 
-describe('cli', () => {
-    it('needs tests');
+test("run error command", async () => {
+  const { stderr } = await bin()("iii");
+  expect(stderr).toContain("未知的命令:iii");
+});
+
+// help
+test("should not error when use --help", async () => {
+  let error = null;
+  try {
+    await bin()("--help");
+  } catch (error) {
+    error = error;
+  }
+  expect(error).toBe(null);
+});
+
+test("package version", async () => {
+  const { stdout } = await bin()("-V");
+  expect(stdout).toContain(require("../package.json").version);
+});
+
+test("is start debug", async () => {
+  let error = null;
+  try {
+    await bin()("--debug");
+  } catch (e) {
+    error = e.message;
+  }
+  expect(error).toContain("launch debug mode");
 });
---
+++ packages/cli/lib/createCli.js
@@ -0,0 +1,44 @@
+import path from "node:path";
+import { program } from "commander";
+import fse from "fs-extra";
+import semver from "semver";
+import chalk from "chalk";
+import { dirname } from "dirname-filename-esm";
+import { log } from "@bubu/utils";
+
+const __dirname = dirname(import.meta);
+const pkgPath = path.resolve(__dirname, "../package.json");
+const pkg = fse.readJSONSync(pkgPath);
+
+const name = Object.keys(pkg.bin)[0];
+const LOWEST_NODE_VERSION = "14.0.0";
+
+function checkNodeVersion() {
+  if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
+    throw new Error(chalk.red(`需要安装${LOWEST_NODE_VERSION}以上版本的node`));
+  }
+}
+
+function preAction() {
+  checkNodeVersion();
+}
+
+export default function createCLI() {
+  program
+    .name(name)
+    .usage("<command> [options]")
+    .version(pkg.version)
+    .option("-d, --debug", "是否开启调试模式", false)
+    .hook("preAction", preAction);
+
+  program.on("option:debug", function () {
+    if (program.opts().debug) {
+      log.verbose("debug", "launch debug mode");
+    }
+  });
+
+  program.on("command:*", function (obj) {
+    log.error("未知的命令:" + obj[0]);
+  });
+  return program;
+}
